7b37a5f8b67204746d6aea8453d9da91dc7aeb1b
Juan Gabriel Diaz <juanbacanp3s@gmail.com>
2024-08-19 11:06:25 -0300

    terminando de editar el nuevo header

--- src/components/PortadaInicio.jsx
+++ src/components/PortadaInicio.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './PortadaInicio.css'; // Asegúrate de que los estilos específicos estén en el archivo CSS
+import Button from 'react-bootstrap/Button';
 
 const PortadaInicio = () => {
   return (
     <div className="contenedorImagen">
       <div className="tituloImagen">
-        <h3>Cuidando de ti, cuidando de tu familia.</h3>
+        <h1>Cuidamos a tus mascotas como familia</h1>
+        <p>
+          En Patitas Felices, nos dedicamos a ofrecer el mejor cuidado para tus mascotas, 
+          con especialistas comprometidos y servicios personalizados.
+        </p>
+        <Button variant="primary" href="#especialistas">
+          Conoce a nuestros especialistas
+        </Button>
+      </div>
+      <div className="contenedorImagenVete">
+        <img src="../src/assets/portada-vete.png" alt="Portada Veterinaria" />
       </div>
     </div>
   );
 };
+
 
 export default PortadaInicio;
